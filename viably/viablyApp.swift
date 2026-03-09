c8f4aad52c28c6ada7c1b6779386cf898b6a9286
Ruitao Chen <ruitaocchen@gmail.com>
2026-03-09 10:19:34 -0400

    Initial Commit

---
+++ viably/viablyApp.swift
@@ -0,0 +1,17 @@
+//
+//  viablyApp.swift
+//  viably
+//
+//  Created by Ruitao Chen on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct viablyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
